89e5724fe0370baf68239a285b34eca15525a08d
shiyasmohd <shiyasjaseena124@gmail.com>
2024-02-06 22:39:15 +0530

    static data image on api - working

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,118 +1,183 @@
-import { GraphQLClient, gql } from "graphql-request";
-import { formatEther } from "viem";
 import { ImageResponse } from "next/og";
-import Image from "next/image";
-import Logo from "../../../public/logo.png"
-import { shortWalletAddress } from "@/lib/helper";
 export const runtime = "edge";
 export const dynamic = "force-dynamic";
+import Logo from "../../../public/logo.png";
 
 // const GRAPHQL_ENDPOINT = `https://gateway-arbitrum.network.thegraph.com/api/${process.env.THE_GRAPH_API_KEY}/subgraphs/id/2hTKKMwLsdfJm9N7gUeajkgg8sdJwky56Zpkvg8ZcyP8`;
-const GRAPHQL_ENDPOINT = "https://api.thegraph.com/subgraphs/name/poap-xyz/poap-xdai"
+// const GRAPHQL_ENDPOINT = "https://api.thegraph.com/subgraphs/name/poap-xyz/poap-xdai"
 
 // const noCacheFetch = async (url: string, options: RequestInit) =>
 //   fetch(url, options);
 
 export async function GET() {
 
-  const address = "0xD8547c84ced4F10A32DC9B5dE4327e36740767C3";
+  //   const address = "0xD8547c84ced4F10A32DC9B5dE4327e36740767C3";
 
-  const document = gql
-    `
-  query {
-    tokens(
-      where: {
-        owner_contains_nocase: "${address}"
-        created_gte: "1672531200"
-        created_lte: "1704067199"
-      }
-    ) {
-      created
-      event {
-        id
-      }
-    }
-  }
-`;
+  //   const document = gql
+  //     `
+  //   query {
+  //     tokens(
+  //       where: {
+  //         owner_contains_nocase: "${address}"
+  //         created_gte: "1672531200"
+  //         created_lte: "1704067199"
+  //       }
+  //     ) {
+  //       created
+  //       event {
+  //         id
+  //       }
+  //     }
+  //   }
+  // `;
 
-  const graphQLClient = new GraphQLClient(GRAPHQL_ENDPOINT, {
-    fetch,
-    cache: "no-store",
-  });
+  //   const graphQLClient = new GraphQLClient(GRAPHQL_ENDPOINT, {
+  //     fetch,
+  //     cache: "no-store",
+  //   });
 
-  const response: any = await graphQLClient.request(document);
-  console.log("Total gnosis POAPS: ", response.tokens.length);
+  //   const response: any = await graphQLClient.request(document);
+  //   console.log("Total gnosis POAPS: ", response.tokens.length);
   const portfolio = 0
 
-  if (response?.sales?.[0]?.nft?.metadata?.svg) {
+
+
+  if (1) {
+    const VALUE_CSS = {
+      fontSize: "2rem",
+      fontWeight: "bold",
+      margin: 0,
+      display: "flex",
+      color: "#ffffff"
+    }
+    const TITLE_CSS = {
+      fontSize: "0.75rem",
+      display: "flex",
+      margin: 0,
+      // color: "#9ca3af"
+      color: "#ffffff"
+    }
+    const BOX_WRAPPER_CSS = { display: "flex", flexDirection: "column", position: "relative", borderRadius: "0.75rem", background: "#18181b" }
+    const BOX_CSS = {
+      borderRadius: "8px",
+      background: "#18181b",
+      color: "#fff",
+      textAlign: "center",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+    }
+
+    console.log("Working")
+
     return new ImageResponse(
       (
-        <div id="divToDownload" className="bg-black p-3">
-          <div className="w-full lg:w-[960px] lg:h-[540px] bg-black rounded-lg grid grid-cols-1 lg:grid-cols-2 gap-2" >
-            <div className=" grid grid-rows-2 gap-2">
-              <div className=" grid grid-cols-2 gap-2">
-                <div className="grid grid-rows-2 gap-2">
-                  <div className="relative h-full w-full rounded-xl bg-zinc-900 shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] before:pointer-events-none before:absolute before:-inset-px before:rounded-xl before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
-                    <div className="box bg-zinc-900 rounded-lg flex justify-center items-center flex-col h-full">
-                      <p className="text-2xl font-bold text-gray-200">10</p>
-                      <p className="text-gray-400 text-xs">Transactions</p>
+        <div style={{ background: "#000", display: "flex", flexDirection: "column", alignItems: "center", padding: "8px" }}>
+          <div style={{
+            display: "flex",
+            borderRadius: "0.5rem",
+            width: "100%",
+            height: "90%",
+            backgroundColor: "#000000",
+            color: "#ffffff",
+          }} >
+
+            {/* Left */}
+            <div style={{
+              display: "flex",
+              flexDirection: "column",
+              paddingRight: "4px",
+              width: "50%",
+            }} >
+              <div style={{
+                display: "flex",
+                width: "100%",
+                paddingBottom: "4px",
+                height: "50%"
+              }} >
+                <div style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  paddingRight: "4px",
+                  // gap: "8px",
+                  width: "50%",
+                  height: "100%",
+                }} >
+                  <div style={{ ...BOX_WRAPPER_CSS as any, width: "100%", height: "113px", borderRadius: "8px", gap: "8px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                    <div style={{ ...BOX_CSS as any }} >
+                      <p style={VALUE_CSS}>17</p>
+                      <p style={TITLE_CSS}>Transactions</p>
                     </div>
                   </div>
-                  <div className="relative h-full w-full rounded-xl bg-zinc-900 shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] before:pointer-events-none before:absolute before:-inset-px before:rounded-xl before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
-                    <div className="box bg-zinc-900 rounded-lg flex justify-center items-center flex-col h-full py-4" >
-                      <p className="text-2xl font-bold text-gray-200">1 $ETH</p>
-                      <p className="text-gray-400 text-xs">Total txn fee paid</p>
+                  <div style={{ ...BOX_CSS as any, height: "114px", borderRadius: "8px", width: "100%", marginTop: "4px" }} >
+                    <div style={BOX_CSS as any} >
+                      <p style={VALUE_CSS}>0.0270 $ETH</p>
+                      <p style={TITLE_CSS}>Total txn fee paid</p>
                     </div>
                   </div>
                 </div>
-                <div className="relative h-full w-full rounded-xl bg-zinc-900 shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] before:pointer-events-none before:absolute before:-inset-px before:rounded-xl before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
-                  <div className="box bg-zinc-900 rounded-lg flex flex-col justify-center items-center gap-10 h-full" >
-                    <div>
-                      <p className="text-2xl font-bold text-gray-200">1</p>
-                      <p className="text-gray-400 text-xs">ETH Sent</p>
+                <div style={{ ...BOX_WRAPPER_CSS as any, width: "50%", paddingLeft: "4px", paddingBottom: "4px" }}>
+                  <div style={{ ...BOX_WRAPPER_CSS as any, width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center", gap: "16px" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                      <p style={VALUE_CSS}>0.0250</p>
+                      <p style={TITLE_CSS}>ETH Sent</p>
                     </div>
-                    <div>
-                      <p className="text-2xl font-bold text-gray-200">2</p>
-                      <p className="text-gray-400 text-xs">ETH Received</p>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                      <p style={VALUE_CSS}>0.0440</p>
+                      <p style={TITLE_CSS}>ETH Received</p>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="relative h-full w-full rounded-xl bg-zinc-900 shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] before:pointer-events-none before:absolute before:-inset-px before:rounded-xl before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
-                <div className="box bg-zinc-900 rounded-lg flex justify-center items-center flex-col h-full" >
-                  <p className="text-3xl font-bold text-gray-200">
-                    {/* <Link href={`https://etherscan.io/address/${data.mostTransactedAddress}`} target="_blank"> */}
-                    {shortWalletAddress("0x06C41df2358deD2Fd891522f9Da75eca2150c10B")}
-                    {/* </Link> */}
+              <div style={{ ...BOX_WRAPPER_CSS as any, paddingTop: "4px", display: "flex", justifyContent: "center", alignItems: "center", width: "100%", height: "50%" }}>
+                <div style={BOX_CSS as any} >
+                  <p style={VALUE_CSS}>
+                    0x06C...0c10B
                   </p>
-                  <p className="text-gray-400 text-xs">Address you most interacted with</p>
+                  <p style={TITLE_CSS}>Address you most interacted with</p>
                 </div>
               </div>
             </div>
-            <div className=" grid grid-rows-2 gap-2">
-              <div className="relative h-full w-full rounded-xl bg-zinc-900 shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] before:pointer-events-none before:absolute before:-inset-px before:rounded-xl before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
-                <div className=" box bg-zinc-900 rounded-lg flex justify-center items-center flex-col h-full">
-                  <p className="text-4xl font-bold text-gray-200">3</p>
-                  <p className="text-gray-400 text-xs">Total NFTs Minted</p>
+
+            {/* Right */}
+            <div style={{
+              display: "flex",
+              flexDirection: "column",
+              paddingLeft: "4px",
+              width: "50%",
+            }}>
+              <div style={{ ...BOX_WRAPPER_CSS as any, paddingBottom: "4px", width: "100%", height: "233px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <div style={BOX_CSS as any}>
+                  <p style={VALUE_CSS} >3</p>
+                  <p style={TITLE_CSS}>Total NFTs Minted</p>
                 </div>
               </div>
-              <div className=" grid grid-cols-2 gap-2">
-                <div className="relative h-full w-full rounded-xl bg-zinc-900 shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] before:pointer-events-none before:absolute before:-inset-px before:rounded-xl before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
-                  <div className="box bg-zinc-900 rounded-lg flex justify-center items-center flex-col h-full py-3">
-                    <p className="text-2xl font-bold text-gray-200">5</p>
-                    <p className="text-gray-400 text-xs">Contracts Deployed</p>
+              <div style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
+                height: "50%",
+                paddingTop: "4px"
+              }}>
+                <div style={{ width: "100%", height: "50%", display: "flex", gap: "4px", paddingBottom: "4px" }}>
+                  <div style={{ ...BOX_WRAPPER_CSS as any, width: "231px", height: "100%", justifyContent: "center", alignItems: "center", paddingRight: "4px" }}>
+                    <div style={{ ...BOX_CSS as any }} >
+                      <p style={VALUE_CSS}>5</p>
+                      <p style={TITLE_CSS}>Contracts Deployed</p>
+                    </div>
+                  </div>
+                  <div style={{ ...BOX_WRAPPER_CSS as any, width: "231px", height: "100%", justifyContent: "center", alignItems: "center", paddingLeft: "4px" }}>
+                    <div style={BOX_CSS as any}>
+                      <p style={VALUE_CSS}>11</p>
+                      <p style={TITLE_CSS}>POAPs Minted</p>
+                    </div>
                   </div>
                 </div>
-                <div className="relative h-full w-full rounded-xl bg-zinc-900 shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] before:pointer-events-none before:absolute before:-inset-px before:rounded-xl before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
-                  <div className=" box bg-zinc-900 rounded-lg flex justify-center items-center flex-col h-full">
-                    <p className="text-2xl font-bold text-gray-200">11</p>
-                    <p className="text-gray-400 text-xs">POAPs Minted</p>
-                  </div>
-                </div>
-                <div className="relative col-span-2 h-full w-full rounded-xl bg-zinc-900 shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] before:pointer-events-none before:absolute before:-inset-px before:rounded-xl before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset] forced-colors:outline">
-                  <div className="box bg-zinc-900 rounded-lg flex justify-center items-center flex-col h-full py-3">
-                    <p className="text-3xl font-bold text-gray-200">{portfolio == 0 ? "" : portfolio > 0 ? "+" : ""}{portfolio.toFixed(2)} %</p>
-                    <p className="text-gray-400 text-xs">
+                <div style={{ ...BOX_WRAPPER_CSS as any, justifyContent: "center", height: "118px", alignItems: "center", paddingTop: "4px" }}>
+                  <div style={{ ...BOX_CSS as any }}>
+                    <p style={VALUE_CSS}>{portfolio == 0 ? "" : portfolio > 0 ? "+" : ""}{portfolio.toFixed(2)} %</p>
+                    <p style={TITLE_CSS}>
                       {
                         portfolio == 0 ? "No Change in Portfolio"
                           : portfolio > 0 ? "Portfolio Increased by"
@@ -124,25 +189,14 @@ export async function GET() {
               </div>
             </div>
           </div>
-          <Image src={Logo} alt="logo" className="w-full max-w-[500px] mx-auto py-4" />
-
-          {/* <div className="flex items-center justify-center mt-4 text-4xl">
-                                <div className={`${aviano.className}  px-4  txt-gradient text-center txt txt-rotate`}>
-                                    ETHEREUM
-                                </div>
-                                <div className={`${aviano.className}  txt-gradient px-4  text-center `}>
-                                    2023
-                                </div>
-
-                                <div className={`${aviano.className}  txt-gradient px-4  text-center `}>
-                                    WRAPPED
-                                </div>
-                            </div> */}
-        </div>
+          <div style={{ width: "100%", height: "10%", display: "flex", justifyContent: "center", alignItems: "center" }} >
+            <img style={{ width: "80%" }} src="https://ipfs.filebase.io/ipfs/QmbsZoaYDuVKDB9apQSJtjLJoictqwY46273GJajoAUuzJ" />
+          </div>
+        </div >
       ),
       {
-        width: 1200,
-        height: 630,
+        width: 960,
+        height: 540,
       }
     );
   } else {
