562445bec5d5ecbed0cc45e610ecebdb5838f899
Hashir <mail2hashirpm@gmail.com>
2024-02-09 20:03:47 +0530

    fix: added env in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
+    ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
+  },
+};
 
 export default nextConfig;
